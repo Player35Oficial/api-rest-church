c13e6caa90df716be427e279c4a6c611d355d1f3
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-08-13 12:34:46 -0300

    feat: yup validations and translates

--- src/server/controllers/dizimo/Create.ts
+++ src/server/controllers/dizimo/Create.ts
@@ -1,13 +1,39 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
 interface IDizimo {
   valor: number;
   categoria: string;
 }
 
-export const create = (req: Request<{}, {}, IDizimo>, res: Response) => {
-  console.log(req.body);
+const bodyValidation: yup.Schema<IDizimo> = yup.object().shape({
+  categoria: yup.string().required().min(5),
+  valor: yup.number().required().moreThan(0),
+});
+
+export const create = async (req: Request<{}, {}, IDizimo>, res: Response) => {
+  let validatedData: IDizimo | undefined;
+
+  try {
+    validatedData = await bodyValidation.validate(req.body, {
+      abortEarly: false,
+    });
+  } catch (error) {
+    const yupError = error as yup.ValidationError;
+    const validationErrors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (error.path === undefined) return;
+      validationErrors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: validationErrors,
+    });
+  }
+
+  console.log(validatedData);
 
   return res.status(StatusCodes.CREATED).send("Create!");
 };
