01b4e3046fb8f2598005f27d2c23e08a4326e8be
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-08-14 09:54:56 -0300

    feat: add transacao subresources endpoints

--- src/server/controllers/transacao/UpdateById.ts
+++ src/server/controllers/transacao/UpdateById.ts
@@ -7,14 +7,24 @@ interface IParamProps {
   id?: number,
 }
 
+interface ITransacao {
+  valor: number;
+  categoria: string;
+}
+
 export const updateByIdValidation = validation(getSchema => ({
   params: getSchema<IParamProps>(yup.object().shape({
     id: yup.number().required().moreThan(0)
+  })),
+  body: getSchema<ITransacao>(yup.object().shape({
+    valor: yup.number().required().moreThan(0),
+    categoria: yup.string().required().min(4)
   }))
 }));
 
 export const updateById = async (req: Request<IParamProps>, res: Response) => {
   console.log(req.params);
+  console.log(req.body);
 
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado!");
 };
--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -12,8 +12,16 @@ router.get("/transacao", TransacoesController.getAllValidation, TransacoesContro
 
 // Todas as transacoes - Dizimo
 router.get("/transacao/dizimo", TransacoesController.getAllValidation, TransacoesController.getAll);
+router.post("/transacao/dizimo", TransacoesController.createValidation, TransacoesController.create);
+router.get("/transacao/dizimo/:id", TransacoesController.getByIdValidation, TransacoesController.getById);
+router.delete("/transacao/dizimo/:id", TransacoesController.deleteByIdValidation, TransacoesController.deleteById);
+router.put("/transacao/dizimo/:id", TransacoesController.updateByIdValidation, TransacoesController.updateById);
 
 // Todas as transacoes - Oferta
 router.get("/transacao/oferta", TransacoesController.getAllValidation, TransacoesController.getAll);
+router.post("/transacao/oferta", TransacoesController.createValidation, TransacoesController.create);
+router.get("/transacao/oferta/:id", TransacoesController.getByIdValidation, TransacoesController.getById);
+router.delete("/transacao/oferta/:id", TransacoesController.deleteByIdValidation, TransacoesController.deleteById);
+router.put("/transacao/oferta/:id", TransacoesController.updateByIdValidation, TransacoesController.updateById);
 
 export { router };
