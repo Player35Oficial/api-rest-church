efd752981d1c39ce33b562949ea57e67825ecdda
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-08-15 12:17:21 -0300

    feat: finally controllers for transacao

--- src/server/controllers/transacao/GetById.ts
+++ src/server/controllers/transacao/GetById.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import * as yup from "yup";
 import { StatusCodes } from "http-status-codes";
 import { validation } from "../../shared/middlewares";
+import { TransacaoProvider } from "../../database/providers";
 
 interface IParamProps {
   id?: number
@@ -14,7 +15,25 @@ export const getByIdValidation = validation(getSchema => ({
 }));
 
 export const getById = async (req: Request<IParamProps>, res: Response) => {
-  console.log (req.params);
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado");
+  if(!req.params.id) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: "O Parâmetro 'id' precisa ser informado"
+      }
+    });
+  }
+  const result = await TransacaoProvider.getById(req.params.id);
+  
+
+  if (result instanceof Error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  } else {
+    return res.status(StatusCodes.OK).send(result);
+  }
+
 };
--- src/server/controllers/transacao/UpdateById.ts
+++ src/server/controllers/transacao/UpdateById.ts
@@ -2,29 +2,45 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
 import { validation } from "../../shared/middlewares";
+import { TransacaoProvider } from "../../database/providers";
+import { TransacaoType, ITransacao } from "../../database/models";
 
 interface IParamProps {
   id?: number,
 }
 
-interface ITransacao {
-  valor: number;
-  categoria: string;
-}
+interface IBodyProps extends Omit<ITransacao, "id_transacao" | "date" | "id_usuario">{}
 
 export const updateByIdValidation = validation(getSchema => ({
   params: getSchema<IParamProps>(yup.object().shape({
     id: yup.number().required().moreThan(0)
   })),
-  body: getSchema<ITransacao>(yup.object().shape({
+  body: getSchema<IBodyProps>(yup.object().shape({
     valor: yup.number().required().moreThan(0),
-    categoria: yup.string().required().min(4)
+    id_tipos_transacao: yup.mixed<TransacaoType>().oneOf(Object.values(TransacaoType)).required(),
   }))
 }));
 
 export const updateById = async (req: Request<IParamProps>, res: Response) => {
-  console.log(req.params);
-  console.log(req.body);
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Não implementado!");
+
+  if (!req.params.id) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      errors: {
+        default: "O parâmetro 'id' é obrigatório"
+      }
+    });
+  }
+
+  const result = await TransacaoProvider.updateById(req.params.id, req.body);
+
+  if (result instanceof Error) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: result.message
+      }
+    });
+  }
+
+  return res.status(StatusCodes.OK).end();
 };
