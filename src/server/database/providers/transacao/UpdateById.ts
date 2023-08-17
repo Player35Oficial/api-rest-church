181630ee9c1ce564bc749c11dc0accc13a6064d8
Yuri Guilherme <yuri.luana2016@gmail.com>
2023-08-17 14:10:01 -0300

    fix: update transacao validation of type

--- src/server/database/providers/transacao/UpdateById.ts
+++ src/server/database/providers/transacao/UpdateById.ts
@@ -6,11 +6,21 @@ import { ITransacao } from "../../models";
 export const updateById = async (id: number, data: Omit<ITransacao, "id" | "id_usuario" | "date">) => {
 
   try {
-    
-    const result = await Knex(ETableNames.transacao).where("id", "=", id).update(data).returning("id");
 
-    if (result) return result;
-    return new Error("Erro ao atualizar registro");
+    const dataReceived = data;
+
+    const repo = await Knex(ETableNames.transacao).where("id", "=", id).first();
+
+    if (dataReceived.id_tipos_transacao === repo.id_tipos_transacao) {
+      const result = await Knex(ETableNames.transacao).where("id", "=", id).first().update(data).returning("id");
+      if (result) return result;
+      return new Error("Erro ao atualizar registro");
+    } else {
+      return new Error(`Você está tentando fazer ${dataReceived.id_tipos_transacao} mas o registro com ID: ${repo.id} é do tipo ${repo.id_tipos_transacao}`);
+    }    
+
+
+    
 
   } catch (error) {
     console.error(error);
