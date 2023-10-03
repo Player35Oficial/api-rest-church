5de1e719cddee41fa7836946e9d74e55f94745d6
Yuri 'Player35' Santana <yuri.luana2016@gmail.com>
2023-10-03 15:04:37 -0300

    fix: modify create controller response

--- src/server/controllers/transacao/Create.ts
+++ src/server/controllers/transacao/Create.ts
@@ -5,28 +5,34 @@ import { validation } from "../../shared/middlewares";
 import { ITransacao, TransacaoType } from "../../database/models";
 import { TransacaoProvider } from "../../database/providers";
 
-interface IBodyProps extends Omit<ITransacao, "id_transacao" | "date">{}
+interface IBodyProps extends Omit<ITransacao, "id_transacao" | "date"> {}
 
 export const createValidation = validation((getSchema) => ({
-  body: getSchema<IBodyProps>(yup.object().shape({
-    id_tipos_transacao: yup.mixed<TransacaoType>().oneOf(Object.values(TransacaoType)).required(),
-    id_usuario: yup.number().required().strict(true),
-    valor: yup.number().positive().required().moreThan(0).strict(true),
-  })),
+  body: getSchema<IBodyProps>(
+    yup.object().shape({
+      id_tipos_transacao: yup
+        .mixed<TransacaoType>()
+        .oneOf(Object.values(TransacaoType))
+        .required(),
+      id_usuario: yup.number().required().strict(true),
+      valor: yup.number().positive().required().moreThan(0).strict(true),
+    })
+  ),
 }));
 
-export const create = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
-
+export const create = async (
+  req: Request<{}, {}, IBodyProps>,
+  res: Response
+) => {
   const result = await TransacaoProvider.create(req.body);
-  
-  
+
   if (result instanceof Error) {
     return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       errors: {
         default: result.message,
-      }
+      },
     });
   }
 
-  return res.status(StatusCodes.CREATED).send("Create!");
+  return res.status(StatusCodes.CREATED).send();
 };
